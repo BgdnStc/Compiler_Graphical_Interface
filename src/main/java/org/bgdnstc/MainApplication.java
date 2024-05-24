a66b075e1925f858b7701a808cf40c8b1958abfd
unknown <bogdanstoica10@gmail.com>
2024-05-24 22:22:13 +0300

    Added text fields

--- src/main/java/org/bgdnstc/MainApplication.java
+++ src/main/java/org/bgdnstc/MainApplication.java
@@ -11,8 +11,11 @@ public class MainApplication extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource(""));
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("MainScene.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("ByteNet Compiler Interface");
+        stage.setScene(scene);
+        stage.show();
     }
 
     public static void main(String[] args) {
