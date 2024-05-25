2f68a9ae118eea961ec62db14c26bd813759ebde
unknown <bogdanstoica10@gmail.com>
2024-05-26 02:45:58 +0300

    Added grid pane

--- src/main/java/org/bgdnstc/MainApplication.java
+++ src/main/java/org/bgdnstc/MainApplication.java
@@ -3,19 +3,41 @@ package org.bgdnstc;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
+import java.io.File;
 import java.io.IOException;
 
 public class MainApplication extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("MainScene.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("GridScene.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
+        Image icon = new Image(new File("src/main/java/org/bgdnstc/ByteNetIcon.png").toURI().toString());
+        System.out.println(icon.getHeight());
+        System.out.println(icon.getWidth());
+        stage.getIcons().add(icon);
         stage.setTitle("ByteNet Compiler Interface");
+        stage.setOnCloseRequest(event -> {
+            event.consume();
+            logout(stage);
+        });
         stage.setScene(scene);
         stage.show();
+    }
+
+    public void logout(Stage stage) {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Close application");
+        alert.setHeaderText("Do you want to close the application?");
+
+        if (alert.showAndWait().get() == ButtonType.OK) {
+            stage.close();
+        }
     }
 
     public static void main(String[] args) {
--- src/main/java/org/bgdnstc/MainController.java
+++ src/main/java/org/bgdnstc/MainController.java
@@ -1,4 +1,60 @@
 package org.bgdnstc;
 
-public class MainController {
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class MainController extends Application {
+    @FXML
+    private GridPane gridPane;
+    @FXML
+    private ImageView imageViewHeader;
+    @FXML
+    private TextArea textAreaSource1;
+    @FXML
+    private TextArea textAreaSource2;
+    @FXML
+    private Button buttonCompile1;
+    @FXML
+    private Button buttonCompile2;
+    @FXML
+    private Button buttonRun1;
+    @FXML
+    private Button buttonRun2;
+
+    @FXML
+    public void initialize() {
+        System.out.println("Initialized.");
+        Image header = new Image(new File("src/main/java/org/bgdnstc/ByteNetHeader.png").toURI().toString());
+        imageViewHeader.setImage(header);
+    }
+
+    @FXML
+    private void saveFile() {
+        System.out.println(textAreaSource1.getParagraphs());
+        try {
+            if (textAreaSource1.getParagraphs() != null) {
+                Files.write(Path.of("source1.txt"), textAreaSource1.getParagraphs());
+            } else {
+                System.out.println("empty");
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
 }
