2a08acde02308c648d5f3a7cdd7932b5f2053b28
unknown <bogdanstoica10@gmail.com>
2024-05-26 20:34:18 +0300

    File chooser added

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module org.bgdnstc.compiler_graphical_interface {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
 
 
     opens org.bgdnstc to javafx.fxml;
--- src/main/java/org/bgdnstc/MainController.java
+++ src/main/java/org/bgdnstc/MainController.java
@@ -7,12 +7,15 @@ import javafx.scene.control.TextArea;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
+import java.awt.*;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Arrays;
 
 public class MainController extends Application {
     @FXML
@@ -35,15 +38,22 @@ public class MainController extends Application {
     @FXML
     public void initialize() {
         System.out.println("Initialized.");
-        Image header = new Image(new File("src/main/java/org/bgdnstc/ByteNetHeader.png").toURI().toString());
+        Image header = new Image(new File("src/main/resources/org/bgdnstc/ByteNetHeader.png").toURI().toString());
         imageViewHeader.setImage(header);
+        openFile();
+    }
+
+    @FXML
+    public void openFile() {
+        FileChooser fileChooser = new FileChooser();
+        File selectedFile = fileChooser.showOpenDialog(new Stage());
     }
 
     @FXML
     private void saveFile() {
-        System.out.println(textAreaSource1.getParagraphs());
+        System.out.println(textAreaSource1.getParagraphs().toArray()[0].toString().length());
         try {
-            if (textAreaSource1.getParagraphs() != null) {
+            if (textAreaSource1.getParagraphs().toArray()[0].toString().isEmpty()) {
                 Files.write(Path.of("source1.txt"), textAreaSource1.getParagraphs());
             } else {
                 System.out.println("empty");
